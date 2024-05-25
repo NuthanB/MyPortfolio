aa48808cc7d49276dea3ce67f2d95ec029066ac6
Nuthan B <NuthanB@protonmail.com>
2024-05-25 15:45:02 +0530

    Home page in the making

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,26 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div>
-          <div>text</div>
-          <div>text</div>
+        <div className="flex flex-col xl:flex-row  items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1">Hello I'm<br/>
+            <span className="text-accent">Nuthan B</span>
+            </h1>
+            <p className="max-w-[500px mb-9 text-white/80">
+              I excel at crafting elegant experiences and I am proficient in various programming
+              languages and technologies
+            </p>
+            <div>
+              <Button>
+                <span>
+                  Download CV
+                </span>
+                <FiDownload/>
+              </Button>
+            </div>
+          </div>
+          <div>Photo</div>
         </div>
       </div>
     </section>
--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             {/* logo */}
             <Link href="/">
             <h1 className="text-4xl font-semibold">
-                NuthanB<span className="text-accent">.</span>
+                Nuthan<span className="text-accent">.</span>
             </h1>
             </Link>
             { /* desktop nav */}
