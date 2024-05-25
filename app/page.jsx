36026ddf9636d85de862ed933fb0f6a971912984
Nuthan B <NuthanB@protonmail.com>
2024-05-25 16:33:45 +0530

    Updated the error

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row  items-center justify-between xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left">
             <span className="text-xl">Software Developer</span>
-            <h1 className="h1">Hello Im<br/>
+            <h1 className="h1">Hello I{"'"}m<br/>
             <span className="text-accent">Nuthan B</span>
             </h1>
             <p className="max-w-[500px mb-9 text-white/80">
